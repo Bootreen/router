75adc55b01ccf223f2d6871a8b456927010357e2
Oleksii Butrin <bootreen@gmail.com>
2024-07-05 08:43:24 +0200

    Cleaned Movies component

--- src/routes/Movies/Movies.jsx
+++ src/routes/Movies/Movies.jsx
@@ -1,18 +1,15 @@
 import "./Movies.css";
 import { useState } from "react";
 import MovieList from "../../components/MovieList/MovieList";
-import MoviePreview from "../../components/MoviePreview/MoviePreview";
 import { Outlet } from "react-router-dom";
 
 const Movies = () => {
-  const [currentMovieId, setCurrentMovieId] = useState(0);
+  const [, setCurrentMovieId] = useState(0);
   const selectMovieHandler = (id) => setCurrentMovieId(id);
 
   return (
     <div className='movie-page-container'>
       <MovieList onSelectMovie={selectMovieHandler} />
-      {/* Don't render preview if no movie selected */}
-      {currentMovieId > 0 && <MoviePreview />}
       <Outlet />
     </div>
   );
